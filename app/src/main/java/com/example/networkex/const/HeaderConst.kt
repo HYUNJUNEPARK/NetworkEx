3997e8f5b0472c29187224feb7bb988e7eda1e5d
HYUNJUNEPARK <june2ac4dev@gmail.com>
2023-06-13 10:14:39 +0900

    Request Header 수정 사항 반영

--- app/src/main/java/com/example/networkex/const/HeaderConst.kt
+++ app/src/main/java/com/example/networkex/const/HeaderConst.kt
@@ -1,19 +1,10 @@
 package com.example.networkex.const
 
 object HeaderConst {
-    const val AUTHORIZATION_NAME = "Authorization"
-
     //KONA
-    const val ASP_NAME = "X-KM-User-AspId"
-    const val ASP_VALUE = "000165000000000"
     const val TRANSACTION_ID = "X-KM-Correlation-Id"
 
-    //SELF CARE
-    const val CLIENT_NAME = "x-kni-client-id"
-    const val CLIENT_VALUE = "b8q1J1N4OKNqWbqhgqOin78sLruoXC"
-    const val CLIENT_SECRET_NAME = "x-kni-client-secret"
-    const val CLIENT_SECRET_VALUE = "SllQ42UsimJdBsFijm21GplpevJ2CatqItVJaZ3X5GRqHqZYZa"
-
     //TOKEN
+    const val AUTHORIZATION_NAME = "Authorization"
     const val ACCESS_TOKEN_BASIC = "Basic bW9uYWFwcDp0aGlzaXNzZWNyZXQ="
 }
