b638ed78e1add27538ebf4d2293e14e6b9c04ae8
HYUNJUNEPARK <june2ac4dev@gmail.com>
2024-01-25 10:29:54 +0900

    feat:loadingView 추가

--- app/src/main/java/com/example/networkex/gson/basic/MainActivity.kt
+++ app/src/main/java/com/example/networkex/gson/basic/MainActivity.kt
@@ -4,14 +4,17 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
 import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
 import com.example.networkex.R
 import com.example.networkex.databinding.ActivityMain3Binding
 import com.example.networkex.gson.basic.model.GitRepository
 import com.example.networkex.gson.basic.network.NetworkManager
+import com.example.networkex.loading.LoadingDialogFragment
 import kotlinx.coroutines.CoroutineExceptionHandler
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -22,6 +25,10 @@ class MainActivity : AppCompatActivity() {
 
     private val networkManager = NetworkManager()
 
+    private val loadingView by lazy {
+        LoadingDialogFragment.newInstance()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main3)
@@ -30,8 +37,15 @@ class MainActivity : AppCompatActivity() {
 
     //enqueue
     fun onTest1ButtonClicked() = CoroutineScope(Dispatchers.IO).launch {
+        withContext(Dispatchers.Main) {
+            loadingView.show(supportFragmentManager, null)
+        }
+
         networkManager.requestGithubInfo("ln-12").enqueue(object : Callback<List<GitRepository>> {
             override fun onResponse(call: Call<List<GitRepository>>, response: Response<List<GitRepository>>) {
+                loadingView.dismiss()
+                Toast.makeText(this@MainActivity, "${response.code()}", Toast.LENGTH_SHORT).show()
+
                 when (response.code()) {
                     200 -> {
                         val responseBody = response.body() as List<GitRepository>
@@ -43,6 +57,7 @@ class MainActivity : AppCompatActivity() {
                 }
             }
             override fun onFailure(call: Call<List<GitRepository>>, t: Throwable) {
+                loadingView.dismiss()
                 Toast.makeText(this@MainActivity, t.message, Toast.LENGTH_SHORT).show()
             }
         })
---
+++ app/src/main/java/com/example/networkex/loading/LoadingDialogFragment.kt
@@ -0,0 +1,70 @@
+package com.example.networkex.loading
+
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.DialogFragment
+import com.example.networkex.R
+import com.example.networkex.databinding.DialogFragmentLoadingBinding
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+
+class LoadingDialogFragment : DialogFragment() {
+    private var _binding : DialogFragmentLoadingBinding? = null
+    private val binding get() = _binding!!
+    private lateinit var imgList: List<Drawable>
+    private lateinit var imgChangeJob: Job
+    private var imgIndexValue = 0
+
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
+        _binding = DataBindingUtil.inflate(inflater, R.layout.dialog_fragment_loading, container, false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        imgList = getLoadingImgList()
+        imgChangeJob = changeImgJob()
+    }
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        imgChangeJob.cancel()
+        _binding = null
+    }
+
+    private fun changeImgJob() = CoroutineScope(Dispatchers.Main).launch {
+        while (true) {
+            delay(300)
+            binding.ivLoading.setImageDrawable(imgList[imgIndexValue++ % imgList.size])
+        }
+    }
+
+    private fun getLoadingImgList(): List<Drawable> {
+        return listOf(
+            ContextCompat.getDrawable(requireActivity(), R.drawable.loading_img_black)!!,
+            ContextCompat.getDrawable(requireActivity(), R.drawable.loading_img_blue)!!,
+            ContextCompat.getDrawable(requireActivity(), R.drawable.loading_img_green)!!,
+            ContextCompat.getDrawable(requireActivity(), R.drawable.loading_img_red)!!
+        )
+    }
+
+    companion object {
+        @JvmStatic
+        @Synchronized
+        fun newInstance(): LoadingDialogFragment {
+            return LoadingDialogFragment().apply {
+                isCancelable = false //시스템 백버튼으로 종료 block
+                setStyle(STYLE_NO_TITLE, R.style.DialogTheme)
+            }
+        }
+    }
+}
